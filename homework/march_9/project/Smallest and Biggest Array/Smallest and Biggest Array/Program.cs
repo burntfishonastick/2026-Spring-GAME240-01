60f2c42bdaab5eae9b91a099fa32d6f429e4e686
burntfishonastick <wnguyen@chapman.edu>
2026-03-12 21:12:06 -0700

    Smallest and Biggest Array

---
+++ homework/march_9/project/Smallest and Biggest Array/Smallest and Biggest Array/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = { 16, 48, 9, 2, 10};
+
+int smallestNum = numbers[0];
+int biggestNum = numbers[0];
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] < smallestNum)
+    {
+        smallestNum = numbers[i];
+    }
+
+    if (numbers[i] > biggestNum)
+    {
+        biggestNum = numbers[i];
+    }
+}
+
+Console.WriteLine( "The smallest number is " + smallestNum + " and the biggest number is " + biggestNum + ".");
