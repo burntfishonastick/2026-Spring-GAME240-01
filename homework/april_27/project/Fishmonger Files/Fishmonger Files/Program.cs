eadbc4599a8804f2d1b4a1a5cececa41265a327e
burntfishonastick <wnguyen@chapman.edu>
2026-04-28 00:04:57 -0700

    Fishmonger Files (partially unfinished?)
    it wont work and i dont know why and im not excited to try and find out

---
+++ homework/april_27/project/Fishmonger Files/Fishmonger Files/Program.cs
@@ -0,0 +1,84 @@
+﻿string specialsfile;
+string logsfile;
+
+//specials
+while (true)
+{
+    Console.WriteLine("What filepath contains today's specials?");
+    specialsfile = Console.ReadLine();
+    try
+    {
+        if (File.Exists(specialsfile))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("No File Found, please try again");
+        }
+    }
+    catch
+    {
+        Console.WriteLine("File does not exist, please try again");
+    }
+}
+
+// logs
+
+while (true)
+{
+    Console.WriteLine("What filepath contains the logs?");
+    logsfile = Console.ReadLine();
+    try
+    {
+        if (File.Exists(logsfile))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("No File Found, please try again");
+        }
+    }
+    catch
+    {
+        Console.WriteLine("File does not exist, please try again");
+    }
+}
+
+Console.WriteLine("Where will the results be saved?");
+string resultsfile = Console.ReadLine();
+
+string specialsName;
+using (StreamReader specialsreader = new StreamReader(specialsfile))
+{
+    string specialsLine = specialsreader.ReadLine().Trim().ToLower();
+    string[] specialsinparts = specialsLine.Split(":");
+    specialsName = specialsinparts[1].Trim();
+}
+int specialsamount = 0;
+int totalfish = 0;
+string specialsName2;
+string[] logs = File.ReadAllLines(logsfile);
+
+foreach (string data in logs)
+{
+    string trimmeddata = data.Trim().ToLower();
+    string[] parts = trimmeddata.Split(' ');
+    string fishName = parts[1];
+    specialsName2 = parts[1];
+    if (fishName == specialsName2)
+    {
+        specialsamount++;
+    }
+}
+
+string results;
+
+using (StreamWriter writer = new StreamWriter(resultsfile))
+{
+    writer.WriteLine($"Today's special is {specialsName}");
+    writer.WriteLine($"Total {specialsName} caught: {specialsamount}");
+    writer.WriteLine($"Total fish caught {totalfish}");
+}
+
